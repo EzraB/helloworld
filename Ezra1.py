67eb92dc20387c994c74f4518f62e0324e75fcfa
Ezra Ahmed <ezra@thisishere.com>
2014-02-23 20:51:19 -0500

    First Commit

---
+++ Ezra1.py
@@ -0,0 +1,30 @@
+print("hello")
+print 8*3253494739
+fred = 100
+print fred
+bob = 210
+print bob
+bob12 = fred
+print bob12
+print 98*61
+number_of_coins = 120
+print number_of_coins
+12335465768*12
+print (bob + fred + bob12 + number_of_coins)
+print (bob + fred)
+dummy = ("GAME OVER!")
+print (dummy)
+poop = ("1-2-3 start!")
+print (poop)
+ezra = ("you win")
+print (ezra)
+fart = '''coin bonus!!
+win a lazer turtle!!!'''
+print fart
+run = '''"You can't poop here!!" he yelled'''
+print run
+run_again = '\"You can\'t poop here!!\" he yelled'
+print run_again
+
+message = "You earned %s coins"
+print (message % bob)
